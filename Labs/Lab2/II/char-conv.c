1c694e76a001a600ee8d4a60a5f819a24a450f19
mabr3 <miguel.b.rodrigues@tecnico.ulisboa.pt>
2018-03-05 15:34:21 +0000

    Lab2 pre-aula

--- Labs/Lab2/II/char-conv.c
+++ Labs/Lab2/II/char-conv.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
+#include <string.h>
 
 int main(){
 	char v1[100];
-	char *v2;
+	char *v2=NULL;
 	int i;
-	
+
 	printf("Write a word");
 	fgets(v1, 100, stdin);
 
-	for (i=0; v1[i]!=0; i+1){
+	v2=(char*) malloc(strlen(v1)*sizeof(char)+1);
+
+	for (i=0; v1[i]!=0; i++){
 		v2[i] = toupper(v1[i]);
 	}
 
 	printf("Converted string: %s", v2);
+	free(v2);
+	exit(0);
 
 
 }
